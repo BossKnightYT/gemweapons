6f4680d8e4e3733bab467ff70256862561c7d066
BossKnightYT <bossknightyt@gmail.com>
2023-04-29 03:06:42 -0400

    Initial commit

---
+++ src/main/java/net/wilson/gemweapons/GemWeaponsClient.java
@@ -0,0 +1,10 @@
+package net.wilson.gemweapons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GemWeaponsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
